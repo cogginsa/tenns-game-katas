a463aa2cc5f1477f1a531028a14a65e5f1c8befb
Abbie Coggins <abbie.coggins@reedbusiness.com>
2020-08-14 15:07:59 +0100

    inital part 2

--- src/test/java/TennisTest.java
+++ src/test/java/TennisTest.java
@@ -77,18 +77,18 @@ public class TennisTest {
         assertEquals(this.expectedScore, game.getScore());
     }
 
-    @Test
-    public void checkAllScoresTennisGame1() {
-        TennisGame1 game = new TennisGame1("player1", "player2");
-        checkAllScores(game);
-    }
-// FOR NEXT SESSION
 //    @Test
-//    public void checkAllScoresTennisGame2() {
-//        TennisGame2 game = new TennisGame2("player1", "player2");
+//    public void checkAllScoresTennisGame1() {
+//        TennisGame1 game = new TennisGame1("player1", "player2");
 //        checkAllScores(game);
 //    }
-//
+// FOR NEXT SESSION
+    @Test
+    public void checkAllScoresTennisGame2() {
+        TennisGame2 game = new TennisGame2("player1", "player2");
+        checkAllScores(game);
+    }
+
 //    @Test
 //    public void checkAllScoresTennisGame3() {
 //        TennisGame3 game = new TennisGame3("player1", "player2");
